fe3069f753136d22519236eb36988e1b8a16f721
vamshishetty26 <vamshishetty26@gmail.com>
2025-01-23 16:18:38 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,126 @@
+//contains() - Checks whether the string contains a substring.
+
+// public class Main {
+//     public static void main(String[] args) {
+//         String myStr = "Hello";
+//         System.out.println(myStr.contains("Hel"));   
+//         System.out.println(myStr.contains("e"));     
+//         System.out.println(myStr.contains("Hi")); 
+//     }
+// }
+
+
+// join() Joins the given strings using the delimiter.
+// public class Main {
+//     public static void main(String[] args) {
+//         String alphabets = String.join(" ", "Abc", "def", "ghi");
+//         System.out.println(alphabets);  
+//     }
+// }
+
+
+
+//charAt() Returns the character present in the specified location.
+// public class Main {
+//     public static void main(String[] args) {
+//       String str1 = "Java ";
+//         System.out.println(str1.charAt(2));
+  
+//     }
+//   }
+
+
+
+// indexOf() Returns the position of the specified character in the string.
+// public class Main {
+//     public static void main(String[] args) {
+//       String str1 = "Java learn";
+//       int result;
+//         result = str1.indexOf('v');
+  
+//       System.out.println(result);
+//     }
+//   }
+
+
+
+
+
+//compareTo() Compares two strings in the dictionary order.
+// public class Main {
+//     public static void main(String[] args) {
+//       String str1 = "Learn Java";
+//       String str2 = "abc Java";
+//       int result;
+//         result = str1.compareTo(str2);
+  
+//       System.out.println(result);
+//     }
+//   }
+
+
+
+//isEmpty() Checks whether a string is empty or not.
+//public class Main {
+//     public static void main(String[] args) {
+  
+//       String str1 = "Learn Java";
+//       String str2 = "";
+  
+//       System.out.println(str1.isEmpty()); 
+//       System.out.println(str2.isEmpty()); 
+//     }
+//   }
+
+//valueOf() Returns the string representation of the specified argument.
+//public class Main {
+//     public static void main(String[] args) {
+  
+//       double interest = 923.234d;
+//         System.out.println(String.valueOf(interest));
+  
+//     }
+//   }
+  
+
+// startsWith() Checks if the string begins with the given string.
+// public class Main {
+//     public static void main(String[] args) {
+  
+//       String str = "JavaScript";
+  
+//       System.out.println(str.startsWith("Java"));
+  
+//     }
+//   }
+  
+
+//endsWith() Checks if the string ends with the given string.
+//public class Main {
+//     public static void main(String[] args) {
+  
+//       String str = "Java lang";
+//       System.out.println(str.endsWith("ang")); // true
+//       System.out.println(str.endsWith("Java")); // false
+//     }
+//   }
+
+
+//subSequence() Returns a subsequence from the string.
+// public class Main {
+//     public static void main(String[] args) {
+//       String str = "Java Programming";
+  
+//       System.out.println(str.subSequence(3, 10)); // a Pro
+//     }
+//   }
+
+public class Main{
+  public static void main(String [] args) {
+    int num1= 2;
+    int num2=4;
+    int num3=5;
+    int sum= num1 + num2 + num3;
+    System.out.println();(sum);
+  }
+}
