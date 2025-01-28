ff381c8e384a23c093700d1b6372e0bc46da48c2
vamshishetty26 <vamshishetty26@gmail.com>
2025-01-28 16:45:56 +0530

    Add files via upload

---
+++ gradingsys.java
@@ -0,0 +1,23 @@
+public class gradingsys {
+        public static String getGrade(int score) {
+            if (score >= 90) {
+                return "A";
+            } else if (score >= 80) {
+                return "B";
+            } else if (score >= 70) {
+                return "C";
+            } else if (score >= 60) {
+                return "D";
+            } else {
+                return "F";
+            }
+        }
+    
+        public static void main(String[] args) {
+            int score = 85;
+            String grade = getGrade(score);
+            System.out.println("Score: " + score + " - Grade: " + grade);
+        }
+    }
+    
+    
