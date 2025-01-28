0944d827a6ba66bc29591cef3da774b713d34876
vamshishetty26 <vamshishetty26@gmail.com>
2025-01-28 16:44:36 +0530

    Add files via upload

---
+++ intdisplay.java
@@ -0,0 +1,11 @@
+class intdisplay{
+    public static int Display()
+    {
+        int a=10;
+        return a;
+    }
+    public static void main(String[] args)
+    {
+        System.out.print(intdisplay.Display());
+    }
+}
