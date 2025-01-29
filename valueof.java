71d86284d4766887e0d1b0cc3a8d343b55e81df6
vamshishetty26 <vamshishetty26@gmail.com>
2025-01-29 22:07:11 +0530

    Add files via upload

---
+++ valueof.java
@@ -0,0 +1,11 @@
+public class valueof {
+
+    public static void main(String[] args) 
+    { 
+        int num = 42; 
+        String str = String.valueOf(num);
+        System.out.println(str); 
+    }
+    
+}
+// this function will give the value of the variable
