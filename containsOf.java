86d75b170ef1900175947816633ed63cde95b185
vamshishetty26 <vamshishetty26@gmail.com>
2025-01-29 22:06:32 +0530

    Add files via upload

---
+++ containsOf.java
@@ -0,0 +1,11 @@
+public class containsOf {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        System.out.println(myStr.contains("Hel"));   
+        System.out.println(myStr.contains("e"));     
+        System.out.println(myStr.contains("Hi")); 
+    }
+
+    
+}
+// this function will return true if the string contains the substring
