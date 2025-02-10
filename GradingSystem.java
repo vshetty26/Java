7ec2ac36f26e241fec1fb8a534cdaa85c27630bc
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-10 16:11:57 +0530

    Add files via upload

---
+++ GradingSystem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GradingSystem {
+
+    public static String getGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the score: ");
+        int score = sc.nextInt();
+        
+        String grade = getGrade(score);
+        System.out.println("Score: " + score + " - Grade: " + grade);
+    }
+}
