82b527a95550ca77b8fce7c69a394eae5ef5c674
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-11 21:19:13 +0530

    Add files via upload

---
+++ add2matrix.java
@@ -0,0 +1,43 @@
+public class add2matrix {
+
+    public static int[][] addMatrices(int[][] matrixA, int[][] matrixB) {
+        int rows = matrixA.length;
+        int cols = matrixA[0].length;
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrixA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrixB = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] sumMatrix = addMatrices(matrixA, matrixB);
+        
+        System.out.println("Resultant Matrix after addition:");
+        printMatrix(sumMatrix);
+    }
+}
