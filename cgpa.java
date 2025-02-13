674c2ae4c6f5120640305f05ca8fb39db3e7ce7c
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-13 22:34:50 +0530

    Add files via upload

---
+++ cgpa.java
@@ -0,0 +1,14 @@
+public class cgpa {
+    public static void main(String args[])
+    {
+        int maths = 30;
+        int c = 30;
+        int java = 30;
+        int total= maths + c+ java;
+        int average = total/3;
+        int cgpa = average/10;
+        System.out.println(cgpa);
+
+    }
+    
+}
