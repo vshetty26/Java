a3161a85ab65d8ee36886a298b487d46418ec8c1
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-17 22:52:13 +0530

    Add files via upload

---
+++ factorialpara.java
@@ -0,0 +1,16 @@
+//factorial using parematerized function
+
+public class factorialpara {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+    public static int fact(int n)
+    {
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+            f=f*i;
+        }
+        return f;
+    }
+}
