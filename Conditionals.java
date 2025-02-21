c5cf82f8c081237e5642148556ba0783ab945f8a
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-21 17:20:39 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,10 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        Double input =-21.19;
+        String output;
+        output=(input>0.0) ? "positive" : "not positive";
+        System.out.println(input + "is" + output);
+    
+}
+
+}
