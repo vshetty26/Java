9a7b119087ebba55e7747474b0b4dae63d4e1e11
vamshishetty26 <vamshishetty26@gmail.com>
2025-02-22 21:21:52 +0530

    Add files via upload

--- GroceryTable.java
+++ GroceryTable.java
@@ -5,17 +5,10 @@ public class GroceryTable {
             {"1", "Maggi Noodles", "Nestle", "15.00"},
             {"2", "Tata Tea", "Tata Global", "98.00"},
             {"3", "Mari biscuit", "Britania", "10.00"},
-            {"4", "Amul butter", "Amul", "45.00"},
-            {"5", "Aashirwad Atta", "ITC", "225.00"},
-            {"6", "Fortune Cooking Oil", "ADANI WILMAR","195.00"},
-            {"7", "Parle-G", "Parle", "5.00"}
         };
-        
 
         System.out.printf("%-10s %-15s %-20s %-10s\n", "Grocery ID", "Item Name", "Manufacturer", "Price");
      
-        
-
         for (String[] item : groceries) {
             System.out.printf("%-10s %-15s %-20s %-10s\n", item[0], item[1], item[2], item[3]);
         }
